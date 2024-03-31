9db45cda5176458b452298f0ba4b76a043cd1987
Remek-P <remek.piotrowski@wp.pl>
2024-03-31 18:13:14 +0200

    added: breakpointXXL; hideItem for medium screens for balancing greed basic grid - done

--- lions-bridge-proposal/pages/grid/grid.js
+++ lions-bridge-proposal/pages/grid/grid.js
@@ -13,28 +13,29 @@ function Grid() {
   return (
       <ul className={classes.grid}>
         <GridItemPicture id="gridItem1" key={1} isPriority={true} path={`${path}logo.jpg`} alt="The right partner" />
-        <GridItemPicture id="gridItem2" key={2} path={`${path}CEO.jpg`} alt="" />
+        <GridItemPicture id="gridItem2" key={2} isPriority={true} path={`${path}CEO.jpg`} alt="CEO" />
         <GridItemPicture id="gridItem3" key={3} path={`${path}office_light.jpg`} alt="" />
         <GridItemPicture id="gridItem4" key={4} isPriority={true} path={`${path}office_mood.jpg`} alt="" />
         <GridItemText id="gridItem5" key={5} textItem="#real estate" alt="#real estate" />
         <GridItemPicture id="gridItem6" key={6} path={`${path}car.jpg`} alt="" />
         <GridItemPicture id="gridItem7" key={7} path={`${path}home_mood.jpg`} alt="" />
         <GridItemText id="gridItem8" key={8} textItem="#people centric" alt="#people centric" />
-        <GridItemPicture id="gridItem9" key={9} path={`${path}laptop.jpg`} />
+        <GridItemPicture id="gridItem9" key={9} path={`${path}laptop.jpg`} alt=""/>
         <GridItemText id="gridItem10" key={10} textItem="#invest" alt="#invest" />
         <GridItemPicture id="gridItem11" key={11} path={`${path}house_on_a_hill.jpg`} alt="" />
         <GridItemPicture id="gridItem12" key={12} path={`${path}helicopter.jpg`} alt="" />
         <GridItemPicture id="gridItem13" key={13} path={`${path}signing.jpg`} alt="" />
         <GridItemPicture id="gridItem14" key={14} path={`${path}couple.jpg`} alt="" />
-        <GridItemPicture id="gridItem15" key={15} path={`${path}office_sunset.jpg`} alt="" />
+        <GridItemPicture id="gridItem15" key={15} hideItem="hideMedium" path={`${path}office_sunset.jpg`} alt="" />
         <GridItemText id="gridItem16" key={16} textItem="#technology" alt="#technology" />
         <GridItemPicture id="gridItem17" key={17} path={`${path}envelope.jpg`} alt="" />
-        <GridItemPicture id="gridItem18" key={18} path={`${path}bridge.jpg`} alt="" />
+        <GridItemPicture id="gridItem18" key={18} hideItem="hideMedium" path={`${path}bridge.jpg`} alt="" />
         <GridItemPicture id="gridItem19" key={19} path={`${path}house_night.jpg`} alt="" />
         <GridItemText id="gridItem20" key={20} textItem="#deal done" alt="#deal Done" />
         <GridItemPicture id="gridItem21" key={21} path={`${path}partnership.jpg`} alt="" />
             <GridItemSurvey id="gridItem22" key={22} />
-        <GridItemPicture id="gridItem23" key={23} hideItem="hide" path={`${path}man.jpg`} alt="" />
+        {/*Only visible on larg screens, so the grid is balanced*/}
+        <GridItemPicture id="gridItem23" key={23} hideItem="hideSmall" path={`${path}man.jpg`} alt="" />
       </ul>
   );
 }
